1dd024ad3b3a634d46f90ec39b9b00b3ed003cc3
eli-jones <140195816+eli-jones@users.noreply.github.com>
2024-02-06 13:04:33 -0700

    Add files via upload

---
+++ intro-sign-up/signup-index.js
@@ -0,0 +1,24 @@
+const inputs = document.getElementsByTagName('input');
+const email = document.getElementById('email');
+var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+function validate() {
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('blur', function () {
+            const error = document.getElementsByClassName('error');
+            const errorIcon = document.getElementsByClassName('error-icon');
+            
+            if (!this.value) {
+                this.classList.add('invalid');
+                error[i].classList.add('visible');
+                errorIcon[i].classList.add('visible');
+            } else {
+                this.classList.remove('invalid');
+                error[i].classList.remove('visible');
+                errorIcon[i].classList.remove('visible');
+            }
+        })
+    }
+}
+
+validate();
